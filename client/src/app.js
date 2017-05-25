44d4bc76b4dcb3bb77a5f6004bdd1cec2c505aa7
Simon Tucker <srtucker22@gmail.com>
2017-05-25 13:42:02 -0500

    Step 1.10: Add ApolloClient

--- client/src/app.js
+++ client/src/app.js
@@ -5,6 +5,42 @@ import {
   View,
 } from 'react-native';
 
+import { ApolloClient } from 'apollo-client';
+import { ApolloLink } from 'apollo-link';
+import { ApolloProvider } from 'react-apollo';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createHttpLink } from 'apollo-link-http';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { ReduxCache, apolloReducer } from 'apollo-cache-redux';
+import ReduxLink from 'apollo-link-redux';
+
+const URL = 'localhost:8080'; // set your comp's url here
+
+const store = createStore(
+  combineReducers({
+    apollo: apolloReducer,
+  }),
+  {}, // initial state
+  composeWithDevTools(),
+);
+
+const cache = new ReduxCache({ store });
+
+const reduxLink = new ReduxLink(store);
+
+const httpLink = createHttpLink({ uri: `http://${URL}/graphql` });
+
+const link = ApolloLink.from([
+  reduxLink,
+  httpLink,
+]);
+
+export const client = new ApolloClient({
+  link,
+  cache,
+});
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
